eaa29e937ea65b6e97361869471f884a341cb240
Anmmoldwivedi89 <anmoldwivedi2002@outlook.com>
2025-05-06 11:55:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+
+async function getSongs() {
+    
+
+let a = await fetch("http://127.0.0.1:5500/songs/")
+let response = await a.text();
+console.log(response);
+let div = document.createElement("div");
+div.innerHTML = response;
+let as = div.getElementsByTagName("a")
+let songs = [];
+for(let index = 0; index< as.length; index++){
+    const element = as[index];
+    if(element.href.endsWith(".mp3")){
+
+    
+    songs.push(element.href.split("/songs/")[1])
+}
+}
+
+return songs;
+}
+let audio = new Audio();
+const playMusic = (track)=> {
+    let audio = new Audio("/songs/" + track)
+    audio.play()
+}
+
+// Get the  list of all the songs
+async function main() {
+    
+let currentSong;
+
+
+    let songs = await getSongs()
+    console.log(songs)
+    
+    //Show all the songs in the playlist
+
+let songUL= document.querySelector(".songList").getElementsByTagName("ul")[0];
+for (const song of songs){
+    songUL.innerHTML = songUL.innerHTML + `<li> 
+    
+    
+            <img class="invert"  src="music.svg" alt="">
+            <div class="info">
+                <div style="width:91px"> ${song.replaceAll("%20", " ")}</div>
+                <div style="width:91px">Anmol</div>
+            </div>
+            <div class="playnow"><span>Play now</span>
+            <img class="invert" src="play.svg" alt="" class="src">
+            </div>
+        </li>`;
+}
+// Attach an event listener to each song
+Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e =>{
+    e.addEventListener("click", element=>{
+console.log(e.querySelector(".info").firstElementChild.innerHTML)
+playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+})
+})
+
+}
+
+
+main();
